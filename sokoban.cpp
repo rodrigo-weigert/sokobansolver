b3f6a5f7e21e95213a561b8424daaf5c32ec3fe7
Rodrigo Weigert <rodrigo.neorooker@gmail.com>
2016-10-05 23:35:38 -0300

    A* except heuristic function.

--- sokoban.cpp
+++ sokoban.cpp
@@ -26,16 +26,6 @@ struct state
 	short boxes[MAX_BOXES_PER_LEVEL];
 };
 
-bool has_box(state& s, short x, short y)
-{
-	for (int i = 0; i < nboxes; i++)
-	{
-		if (get_x(s.boxes[i]) == x and get_y(s.boxes[i]) == y)
-			return true;
-	}
-	return false;
-}
-
 struct state_cmp
 {
 	bool operator() (const state& a, const state& b) const
@@ -54,6 +44,36 @@ struct state_cmp
 		return false;
 	}
 };
+
+struct anode		//É um nó da fila de prioridade a ser utilizada pelo A*
+{
+	short cost;		// custo do caminho até agora
+	short est;		// estimativa de custo até o objetivo
+	state s;		// estado associado
+};
+
+struct anode_cmp
+{
+	bool operator() (const anode& a, const anode& b) const
+	{		
+		/* Um anode (nó que envelopa um estado) é considerado 'menor'
+		que outro se o valor de sua função heurística
+		(custo do passado + estimativa do futuro) tem valor maior
+		que a do outro. A fila de prioridade do A* colocará em seu topo
+		os  maiores estados (envelopados em anodes). */
+		return (a.cost + a.est) > (b.cost + b.est);
+	}
+};
+
+bool has_box(state& s, short x, short y)
+{
+	for (int i = 0; i < nboxes; i++)
+	{
+		if (get_x(s.boxes[i]) == x and get_y(s.boxes[i]) == y)
+			return true;
+	}
+	return false;
+}
 
 bool valid(short x, short y)
 {
@@ -131,11 +151,11 @@ bool equal(const state& a, const state& b)
 
 string bfs(state& initial)
 {
-	map<state, state, state_cmp> m;
+	map<state, state, state_cmp> prev;
 	queue<state> q;
 	state end;
 
-	m[initial] = initial;
+	prev[initial] = initial;
 	q.push(initial);
 	while (!q.empty())
 	{
@@ -149,10 +169,10 @@ string bfs(state& initial)
 		for (int d = 0; d < 4; d++)
 		{
 			state next = cur;
-			if(move(next, d) and !m.count(next))
+			if(move(next, d) and !prev.count(next))
 			{
 				q.push(next);
-				m[next] = cur;
+				prev[next] = cur;
 			}
 		}
 	}
@@ -160,7 +180,77 @@ string bfs(state& initial)
 	state cur = end;
 	while (!equal(cur, initial))
 	{
-		state prv = m[cur];
+		state prv = prev[cur];
+		for (int d = 0; d < 4; d++)
+		{
+			if (prv.px + dir[d][0] == cur.px and prv.py + dir[d][1] == cur.py)
+			{
+				path.push_back(dirname[d]);
+				break;
+			}
+		}
+		cur = prv;
+	}
+	reverse(path.begin(), path.end());
+	return path;
+}
+
+short estimate(state& s)
+{
+	/* Será a função heurística de estimativa
+	para número de movimentos faltando para o objetivo 
+	dado um estado. */
+	return 0;
+}
+
+string astar(state& initial)
+{
+	map<state, state, state_cmp> prev;
+	priority_queue<anode, vector<anode>, anode_cmp> q;
+	map<state, short, state_cmp> dist;
+	state end;
+	anode initial_a;
+
+	initial_a.cost = 0;
+	initial_a.est = estimate(initial);
+	initial_a.s = initial;
+
+	prev[initial] = initial;
+	dist[initial] = 0;
+	q.push(initial_a);
+	while (!q.empty())
+	{
+		anode cur = q.top();
+		q.pop();
+
+		if (cur.cost > dist[cur.s])
+			continue;
+
+		if (cleared(cur.s))
+		{
+			end = cur.s;
+			break;
+		}
+		for (int d = 0; d < 4; d++)
+		{
+			state next = cur.s;
+			if (move(next, d) and (!dist.count(next) or dist[next] > cur.cost + 1))
+			{
+				anode next_a;
+				next_a.s = next;
+				next_a.cost = dist[next] = cur.cost + 1;
+				next_a.est = estimate(next);
+				prev[next] = cur.s;
+				q.push(next_a);
+			}
+		}
+	}
+	/* A parte abaixo (reconstrução do caminho) é identica a da BFS */
+	string path;
+	state cur = end;
+	while (!equal(cur, initial))
+	{
+		state prv = prev[cur];
 		for (int d = 0; d < 4; d++)
 		{
 			if (prv.px + dir[d][0] == cur.px and prv.py + dir[d][1] == cur.py)
@@ -195,6 +285,7 @@ int main()
 				initial.boxes[nboxes++] = get_pos(i, j);
 		}
 	}
-	printf("%s\n", bfs(initial).c_str());
+	printf("BFS:\t%s\n", bfs(initial).c_str());
+	printf("A*:\t%s\n", astar(initial).c_str());
 	return 0;
 }
